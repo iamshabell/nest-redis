6bb23d7d61448562606a4bc27db03deec96d1f40
iamshabell <91321698+iamshabell@users.noreply.github.com>
2022-04-22 13:54:56 +0300

    feat(redis): implemented redis

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,23 @@
 import { Controller, Get } from '@nestjs/common';
+import { EventPattern, MessagePattern } from '@nestjs/microservices';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
+  @Get('/greeting')
+  async getHello() {
     return this.appService.getHello();
+  }
+
+  @Get('/greeting-async')
+  async getHelloAsync() {
+    return this.appService.getHelloAsync();
+  }
+
+  @Get('/publish-event')
+  async publishEvent() {
+    this.appService.publishEvent();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(@Inject('GREETING_SERVICE') private client: ClientProxy) {}
+
+  async getHello() {
+    return this.client.send({ cmd: 'greeting' }, 'Progressive Coder');
+  }
+
+  async getHelloAsync() {
+    const message = await this.client.send(
+      { cmd: 'greeting-async' },
+      'Progressive Coder',
+    );
+    return message;
+  }
+
+  async publishEvent() {
+    this.client.emit('book-created', {
+      bookName: 'The Way Of Kings',
+      author: 'Brandon Sanderson',
+    });
   }
 }
